ba0846a0d1b57f85dc1c57a978b63ca5f6823d68
darklinden <darklinden@gmail.com>
2023-11-16 18:57:06 +0800

    add bin2hex

--- bin2hex/main.rs
+++ bin2hex/main.rs
@@ -13,13 +13,12 @@ fn main() {
     }
 
     let input_file_name = &args[1];
-    let output_file_name = format!("{}.bin", input_file_name);
+    let output_file_name = format!("{}.txt", input_file_name);
     if Path::new(&output_file_name).exists() {
-        println!("{} already exists", output_file_name);
+        println!("{} already exists, remove", output_file_name);
         fs::remove_file(&output_file_name).expect("remove file failed");
     }
 
-    // println!("Hello, world!");
     // read a utf-8 string file and convert every 2 char hex to bytes, write out to a file
     let in_file = File::open(&input_file_name).expect("file not found");
     let mut buf_reader = BufReader::new(in_file);
@@ -27,11 +26,11 @@ fn main() {
     let out_file = File::create(&output_file_name).expect("create file failed");
     let mut buf_writer = BufWriter::new(out_file);
 
-    let mut tmp: Vec<char> = Vec::new();
     let mut u8arr: [u8; 1] = [0];
 
     let mut index: u64 = 0;
     let mut count: u64 = 0;
+    let mut line_count: u64 = 0;
     loop {
         let size = buf_reader.read(&mut u8arr);
         if size.is_err() {
@@ -44,38 +43,24 @@ fn main() {
         }
 
         index += 1;
-        let c = u8arr[0] as char;
+        let c = u8arr[0];
 
-        // should skip \r \n 0x space
-        if c == '\n' || c == '\r' || c == ' ' {
-            continue;
-        }
+        count += 3;
+        line_count += 1;
+        buf_writer
+            .write_fmt(format_args!("{:02x} ", c))
+            .expect("write file failed");
 
-        tmp.push(c);
-
-        if tmp.len() == 2 {
-            if tmp[0] == '0' && tmp[1] == 'x' {
-                tmp = Vec::new();
-                continue;
-            }
-
-            let s: String = tmp.into_iter().collect();
-            let b = u8::from_str_radix(&s, 16);
-            if b.is_err() {
-                println!("{}: {} is not a hex number", index, s);
-                return;
-            }
-
-            let b = b.unwrap();
+        if line_count == 16 {
+            line_count = 0;
             count += 1;
-            buf_writer.write(&[b]).expect("write file failed");
-            tmp = Vec::new();
+            buf_writer.write(b"\n").expect("write file failed");
         }
     }
     buf_writer.flush().expect("flush failed");
 
     println!(
-        "{} chars read and {} bytes written to {}",
+        "{} bytes read and {} chars written to {}",
         index, count, output_file_name
     );
 }
--- test/test.sh
+++ test/test.sh
@@ -6,4 +6,5 @@ PROJCET_DIR="$(realpath "${TEST_DIR}/..")"
 
 cd $PROJCET_DIR
 
-cargo r test/1.txt
+cargo r --bin hex2bin ./test/1.txt
+cargo r --bin bin2hex ./test/1.txt.bin
